dd8d2703d90bf0bb86c20f2c82d40513e74cae75
Laura Sofia Rebolledo <lsrasofia2003@gmail.com>
2021-11-19 19:46:45 -0500

    Merge origin/master
    Conflicts:
    	FlightsBooking/nbproject/project.properties
    	FlightsBooking/src/Main.java

--- FlightsBooking/src/Principal/Main.java
+++ FlightsBooking/src/Principal/Main.java
@@ -8,5 +8,16 @@ public class Main {
     public static void main(String[] args) {
         VistaInicial Vista=new VistaInicial();
         Vista.setVisible(true);
+        Vista.getPanelInicial().setVisible(false);
+        Vista.getPanelRegistro().setVisible(false);
+        Vista.getPanelAdministrador().setVisible(false);
+        Vista.getPanelPrincipal().setVisible(true);
+        Vista.getPanelOpcionesAdministrador().setVisible(false);
+        Vista.getPanelMostrarAdministrador().setVisible(false);
+        Vista.getPanelAyuda().setVisible(false);
+        Vista.getPanelNotificacion().setVisible(false);
+        Vista.getPanelCuenta().setVisible(false);
+        Vista.getPanelModificarDatos().setVisible(true);
+        Vista.getPanelInternoModificarDatos().setVisible(false);
     }
 }
